2f160aa471e4f88e04f45837327a33b24a598566
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-10-30 14:04:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /*
- * S3rver on Deta Micro
- * NOTE: all data is EPHEMERAL until deta base storage is implemented
+ * S3rver on Deta Micro w/ Deta Base (metadata) + Drive (files) storage
+ * 
+ * ENV BUCKETS = comma separated buckets (test,default,etc)
+ * ENV DETA_TOKEN = provided by Micro runtime
+ * ENV DETA_RUNTIME = provided by Micro runtime
+ * 
  */
 
 const express = require('express')
 const app = express()
-const S3rver = require('s3rver');
+const S3rver = require('@qxip/s3rver');
 
 // Set Buckets from BUCKETS ENV csv
 var BUCKETS = process.env.BUCKETS.split(',').map(name => ({name})) || false;
@@ -20,9 +24,7 @@ const s3rver = new S3rver({
 }).run(function(){ console.log('s3rver running')});
 
 // S3 API middleware
-app.get('*', (req, res) => { s3rver.getMiddleware()(req, res) })
-app.put('*', (req, res) => { s3rver.getMiddleware()(req, res) })
-app.post('*',(req, res) => { s3rver.getMiddleware()(req, res) })
+app.any('*', (req, res) => { s3rver.getMiddleware()(req, res) })
 
 const isMicro = process.env.DETA_RUNTIME || process.env.DETA_RUNTIME || false;
 if (!isMicro) app.listen({port: process.env.PORT || 4568, host: process.env.HOST || "0.0.0.0"})

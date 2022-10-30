81d7c5b1c0ee8e6b9f252c01a1744b5fd4c584e3
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-10-30 15:39:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const S3rver = require('@qxip/s3rver');
 
 // Set Buckets from BUCKETS ENV csv
@@ -23,8 +24,12 @@ const s3rver = new S3rver({
     configureBuckets: BUCKETS || [{ name: 'test'}]
 }).run(function(){ console.log('s3rver running')});
 
+// CORS middleware
+app.use(cors());
 // S3 API middleware
-app.all('*', (req, res) => { s3rver.getMiddleware()(req, res) })
+app.all('*', (req, res) => {
+        s3rver.getMiddleware()(req, res);
+})
 
 const isMicro = process.env.DETA_RUNTIME || process.env.DETA_RUNTIME || false;
 if (!isMicro) app.listen({port: process.env.PORT || 4568, host: process.env.HOST || "0.0.0.0"})
